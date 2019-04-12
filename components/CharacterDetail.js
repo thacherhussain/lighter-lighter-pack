93265c201681999775703aadc8c4bbf6210c731a
Thacher Hussain <thacherT1D@gmail.com>
2019-04-12 16:30:39 -0700

    formatted data and images

--- components/CharacterDetail.js
+++ components/CharacterDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Image } from 'react-native';
 import {
   Card,
   CardItem,
@@ -6,16 +7,35 @@ import {
 } from 'native-base';
 
 const CharacterDetail = ({ character }) =>  {
-  const { name, house, role } = character;
+  const { name, house, role, image_url } = character;
   return (
     <Card>
-      <CardItem>
-        <Text>{name}</Text>
-        <Text>{house}</Text>
-        <Text>{role}</Text>
-      </CardItem>
+        <CardItem header>
+          <Text>{name}</Text>
+        </CardItem>
+        <CardItem cardBody>
+          <Image source={{uri: image_url}} style={{height: 400, width: null, flex: 1}}/>
+        </CardItem>
+        <CardItem>
+          <Text>{image_url}</Text>
+        </CardItem>
+        <CardItem>
+          <Text>House: {house}</Text>
+        </CardItem>
+        <CardItem>
+          <Text>Role: {role}</Text>
+        </CardItem>
     </Card>
   );
 };
+
+const styles = {
+  nameTextStyle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    margin: 15,
+    whiteSpace: 'pre-line',
+  }
+}
 
 export default CharacterDetail;
