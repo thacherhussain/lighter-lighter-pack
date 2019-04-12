f4d5c88221ca498a2aa350c8ab5f8f3d4f928fef
Thacher Hussain <thacherT1D@gmail.com>
2019-04-12 12:31:41 -0700

    re-org pre-axios

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,30 +1,21 @@
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-  Alert,
-} from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
 
-import TodoApp from '../components/TodoApp'
-import store from '../store'
-import { Provider } from 'react-redux'
-
-import { WebBrowser } from 'expo';
+// import { WebBrowser } from 'expo';
 
 import {
   Container,
-  Header,
   Content,
   Button,
   Text,
   Form,
   Item,
   Input,
- } from 'native-base';
+} from 'native-base';
+
+import TodoApp from '../components/TodoApp';
+import store from '../store';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -36,29 +27,7 @@ export default class HomeScreen extends React.Component {
 
       <Container>
         <Content padder>
-          <Provider store={store}>
-            <TodoApp />
-          </Provider>
-
-          <Button
-            block
-            onPress={() => this.props.navigation.navigate('LinksStack')}
-            style={styles.button}>
-            <Text style={{textAlign: 'center'}}>Links</Text>
-          </Button>
-          <Form>
-            <Item>
-              <Input placeholder="Username"/>
-            </Item>
-            <Item last>
-              <Input placeholder="Password" />
-            </Item>
-          </Form>
-          <Button
-            block
-            onPress={() => this.props.navigation.navigate('LinksStack')}>
-            <Text style={{textAlign: 'center'}}>Sign In</Text>
-          </Button>
+          <Text>Hello</Text>
         </Content>
       </Container>
 
@@ -66,14 +35,10 @@ export default class HomeScreen extends React.Component {
   }
 }
 
-const showAlert = () =>{
-   Alert.alert(
-      'Alert!'
-   )
-}
 
 const styles = StyleSheet.create({
-  button: {
+  redButton: {
   backgroundColor: '#BF3A2C',
+  margin: 5,
   },
 });
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, TouchableOpacity, Alert } from 'react-native';
-import { Container, Header, Content, Button, Text } from 'native-base';
+import { StyleSheet, Alert } from 'react-native';
+import { Container, Header, Content, Button, Text, Form, Item, Input } from 'native-base';
+import { Provider } from 'react-redux';
+
+import TodoApp from '../components/TodoApp';
+import store from '../store';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -14,37 +18,55 @@ export default class LinksScreen extends React.Component {
       <Container>
         <Header />
         <Content padder>
-          <Button block onPress={showAlert} style={styles.button}>
-            <Text style={{textAlign: 'center'}}>Click Me!</Text>
+          <Button block onPress={showAlert} style={[styles.button, styles.redButton]}>
+            <Text style={{ textAlign: 'center' }}>Click Me!</Text>
           </Button>
-        </Content>
-        <Content padder>
           <Button
             block
             onPress={() => this.props.navigation.navigate('HomeStack')}
-            style={styles.otherButton}>
+            style={[styles.button, styles.pinkButton]}>
               <Text>Home</Text>
           </Button>
+        
+          <Provider store={store}>
+            <TodoApp />
+          </Provider>
+
+          <Form>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+          </Form>
+          <Button
+            block
+            onPress={() => this.props.navigation.navigate('LinksStack')}>
+            <Text style={{ textAlign: 'center' }}>Sign In</Text>
+          </Button>
+
+
         </Content>
       </Container>
     );
   }
 }
 
-const showAlert = () =>{
+const showAlert = () => {
    Alert.alert(
       'Alert!'
-   )
-}
+   );
+};
 
 const styles = StyleSheet.create({
   button: {
+    margin: 5,
+  },
+  redButton: {
     backgroundColor: '#BF3A2C',
   },
-  otherButton: {
+  pinkButton: {
     backgroundColor: '#E2564C',
-  },
-  contentSpacing: {
-    justifyContent: 'space-between',
   },
 });
