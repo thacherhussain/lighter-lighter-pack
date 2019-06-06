77a7021b090db894dc8b6777a91b394001e9e31a
Thacher Hussain <thacherT1D@gmail.com>
2019-06-06 12:44:25 -0700

    nav and linting updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,5 @@
+"use strict";
+
 module.exports = {
-  "extends": "rallycoding",
-  "rules": {
-    "react/jsx-closing-bracket-location": "disabled"
-  }
-  "env": {
-    "browser": true
-  },
-  "globals": {
-      "fetch": true
-    }
+  extends: ["rallycoding"]
 };
--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-import MainTabNavigator from './MainTabNavigator';
-import GearStackNavigator from './GearStackNavigator';
+import MainTabNavigator from "./MainTabNavigator";
+import GearStackNavigator from "./GearStackNavigator";
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator, Gear: GearStackNavigator,
-}));
+export default createAppContainer(
+  createSwitchNavigator({
+    // You could add another route here for authentication.
+    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+    Main: MainTabNavigator,
+    Gear: GearStackNavigator
+  })
+);
